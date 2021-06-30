ff5513519d6d60f33aecab2599385b88fdf2d083
tanloc <tanloc6998@gmail.com>
2021-06-30 18:29:33 +0700

    add component CinemaSeat

---
+++ src/Components/CinemaSeats/index.js
@@ -0,0 +1,142 @@
+import React from "react";
+import "./css.css";
+
+export default function CinemaSeat() {
+  return (
+    <div className="theatre">
+      <div className="cinema-screen">
+        <p>SCREEN</p>
+      </div>
+      <div className="cinema-seats left">
+        <div className="cinema-row row-1">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-2">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-3">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-4">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-5">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-6">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-7">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+      </div>
+      <div className="cinema-seats right">
+        <div className="cinema-row row-1">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-2">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-3">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-4">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-5">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-6">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+        <div className="cinema-row row-7">
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+          <div className="seat" />
+        </div>
+      </div>
+    </div>
+  );
+}
