0804330e3fe1529dacd16ad05edc5f17925f6ed1
tanloc <tanloc6998@gmail.com>
2021-07-04 08:31:12 +0700

    add component Footer

--- src/Container/HomeTemplate/HomePage/index.js
+++ src/Container/HomeTemplate/HomePage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "../../../Components/SliderCarousel";
 import Film from "../../../Components/Film"
 import Cinema from "../../../Components/Cinema"
+import Footer from "../../../Components/Footer"
 
 export default function HomePage() {
   return (
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Carousel />
       <Film/>
       <Cinema/>
+      <Footer/>
     </div>
   );
 }
